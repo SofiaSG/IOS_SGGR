4597219483b92ef42b20678d7d1d33f8223be2f8
SofiaSG <sofylu_56@hotmail.com>
2014-03-06 21:04:49 -0600

    Initial Commit

---
+++ Dog_Animation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dog_Animation
+//
+//  Created by Sofia Garcia Ramos on 3/6/14.
+//  Copyright (c) 2014 Sofia Garcia Ramos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
