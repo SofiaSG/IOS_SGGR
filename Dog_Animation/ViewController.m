d904d8e0f7d219833752a4a5adaeed3bb58edbc5
SofiaSG <sofylu_56@hotmail.com>
2014-03-06 21:26:39 -0600

    Look animation in app

--- Dog_Animation/ViewController.m
+++ Dog_Animation/ViewController.m
@@ -18,6 +18,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSArray * imageArray  = [[NSArray alloc] initWithObjects:
+                             
+                             [UIImage imageNamed:@"dog1.tiff"],
+                             [UIImage imageNamed:@"dog2.tiff"],
+                             [UIImage imageNamed:@"dog3.tiff"],
+                             [UIImage imageNamed:@"dog4.tiff"],
+                             [UIImage imageNamed:@"dog5.tiff"],
+                             [UIImage imageNamed:@"dog6.tiff"],
+                             [UIImage imageNamed:@"dog7.tiff"],
+                             [UIImage imageNamed:@"dog8.tiff"],
+                             [UIImage imageNamed:@"dog9.tiff"],
+                             [UIImage imageNamed:@"dog10.tiff"],
+                             nil];
+    
+	UIImageView * ryuJump = [[UIImageView alloc] initWithFrame:
+                             CGRectMake(100, 125, 150, 130)];
+	ryuJump.animationImages = imageArray;
+	ryuJump.animationDuration = 1.1;
+	ryuJump.contentMode = UIViewContentModeBottomLeft;
+	[self.view addSubview:ryuJump];
+	[ryuJump startAnimating];
 }
 
 - (void)didReceiveMemoryWarning
